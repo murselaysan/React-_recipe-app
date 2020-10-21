b4659f3ca1ae6ca71e554107fa84b7613ad99df7
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-10-21 21:27:45 +0100

    Adding some css

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
-export default function Recipe({title,calories,image}) {
+import style from './recipe.module.css';
+
+export default function Recipe({title,calories,image,ingredients}) {
     return (
-        <div>
+        <div className ={style.recipe}>
             <h1>{title}</h1>
+            <ol>
+                {ingredients.map(ingredient =>(
+                    <li>{ingredient.text}</li>
+                ))}
+            </ol>
             <p>{calories}</p>
-            <img src ={image} alt= ""/>
+            <img className ={style.image} src ={image} alt= ""/>
         </div>
     )
 }
