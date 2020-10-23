2239aaac773f5c328c776dffdaf847a5a1310d99
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-10-23 22:59:30 +0100

    update app css

--- src/Recipe.js
+++ src/Recipe.js
@@ -5,7 +5,7 @@ import style from './recipe.module.css';
 export default function Recipe({title,calories,image,ingredients}) {
     return (
         <div className ={style.recipe}>
-            <h1>{title}</h1>
+            <h1 className ="recipe-title">{title}</h1>
             <ol>
                 {ingredients.map(ingredient =>(
                     <li>{ingredient.text}</li>
